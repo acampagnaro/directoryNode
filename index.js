2a3ef8792ef0000ccf04f527384b0e35da6200d2
alexandre campagnaro <alexandre.campagnaro>
2019-11-19 10:58:58 -0300

    final

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const  path  = require('path')
 const fsExtra = require("fs-extra")
 const moment = require('moment')
+const rimraf = require("rimraf")
+
 let iCount
 
 const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
@@ -9,25 +11,28 @@ const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isD
 const directories = dirs(__dirname  + '/folders')
 
 // copy source folder to destination
-// directories.map((item) => {
-//   fsExtra.copy(__dirname + '/folders/' + item + '/Arquivos', __dirname + '/folders/' + item, function (err) {
-//     if (err) {
-//       return console.error(err)
-//     }
-//     console.log('Copy completed!')
-//   })
-// })
-
-fs.readdir(__dirname + '/folders/22', (err, files) => {
-  if (err) console.log('ERROR: ' + err)
-  iCount = 0
-  files.forEach(file => {
-    if (file === 'Arquivos') return
-    iCount++
-    const extnameFile = path.extname(file)
-    const nameFile = '22' + "_" + moment().format('DDMMYYYY') + "_0" + iCount + extnameFile
-    fs.rename(__dirname + '/folders/22/' + file, __dirname + '/folders/22/' + nameFile, function (err) {
-      if (err) return console.log(err)
+directories.map((item) => {
+  fsExtra.copy(__dirname + '/folders/' + item + '/Arquivos', __dirname + '/folders/' + item, function (err) {
+    if (err) {
+      return console.error(err)
+    }
+    fs.readdir(`${__dirname}/folders/${item}`, (err, files) => {
+      if (err) console.log('ERROR: ' + err)
+      iCount = 0
+      files.forEach(file => {
+        if (file === 'Arquivos') return
+        iCount++
+        const extnameFile = path.extname(file)
+        const nameFile = item + "_" + moment().format('DDMMYYYY') + "_0" + iCount + extnameFile
+        fs.rename(`${__dirname}/folders/${item}/${file}`, `${__dirname}/folders/${item}/${nameFile}`, (err) => {
+          if (err) return console.log(err)
+        })
+        rimraf(`${__dirname}/folders/${item}/Arquivos`, function (err) {
+          if (err) console.log(err)
+          console.log("done")
+        })
+      })
     })
+    console.log('Copy completed!')
   })
 })
