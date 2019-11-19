8f207cc15a07d48cd0b014b6a13bfce23b1c23e8
alexandre campagnaro <alexandre.campagnaro>
2019-11-19 09:48:09 -0300

    funcionando

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isD
 const directories = dirs(__dirname  + '/folders')
 
 // copy source folder to destination
-directories.map((item) => {
-  fsExtra.copy(__dirname + '/folders/' + item + '/Arquivos', __dirname + '/folders/' + item, function (err) {
-    if (err) {
-      return console.error(err)
-    }
-    console.log('Copy completed!')
-  })
-})
+// directories.map((item) => {
+//   fsExtra.copy(__dirname + '/folders/' + item + '/Arquivos', __dirname + '/folders/' + item, function (err) {
+//     if (err) {
+//       return console.error(err)
+//     }
+//     console.log('Copy completed!')
+//   })
+// })
 
 fs.readdir(__dirname + '/folders/22', (err, files) => {
+  if (err) console.log('ERROR: ' + err)
   iCount = 0
   files.forEach(file => {
     if (file === 'Arquivos') return
     iCount++
     const extnameFile = path.extname(file)
     const nameFile = '22' + "_" + moment().format('DDMMYYYY') + "_0" + iCount + extnameFile
-    fs.rename(file, nameFile, function (err) {
-      if (err) console.log('ERROR: ' + err)
-        console.log(file)
+    fs.rename(__dirname + '/folders/22/' + file, __dirname + '/folders/22/' + nameFile, function (err) {
+      if (err) return console.log(err)
     })
   })
 })
